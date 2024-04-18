6d579b2e31a08196cdfea9c9ba603b55cf750ebb
Yasunari Osakabe <yasunari.osakabe@init-inc.com>
2024-04-19 07:42:35 +0900

    Initial Commit

---
+++ Yumemi-Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yumemi-Training
+//
+//  Created by 小坂部泰成 on 2024/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
